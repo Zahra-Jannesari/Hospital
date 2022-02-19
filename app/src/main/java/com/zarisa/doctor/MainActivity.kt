0717f44b5c8f35c67b7a720285ec313bf9dc66e0
Zarisa <jannesari.zarisa@gmail.com>
2022-02-19 15:03:46 +0330

    setOnClickListeners

--- app/src/main/java/com/zarisa/doctor/MainActivity.kt
+++ app/src/main/java/com/zarisa/doctor/MainActivity.kt
@@ -3,9 +3,10 @@ package com.zarisa.doctor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import com.zarisa.doctor.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding:ActivityMainBinding
     var menuClicked:Boolean=false
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,8 +15,17 @@ class MainActivity : AppCompatActivity() {
         val view=binding.root
         setContentView(view)
 
-        binding.buttonMenu.setOnClickListener {menuButtonOnClick()}
+        buttonsOnClickListeners()
+
     }
+
+    private fun buttonsOnClickListeners() {
+        binding.buttonMenu.setOnClickListener { menuButtonOnClick() }
+        binding.buttonFirstButtonOfMenu.setOnClickListener(this)
+        binding.buttonSecondButtonOfMenu.setOnClickListener(this)
+        binding.buttonThirdButtonOfMenu.setOnClickListener(this)
+    }
+
     fun menuButtonOnClick(){
         if (!menuClicked) {
             binding.buttonThirdButtonOfMenu.visibility = View.VISIBLE
@@ -28,5 +38,9 @@ class MainActivity : AppCompatActivity() {
             binding.buttonFirstButtonOfMenu.visibility = View.GONE
             menuClicked=!menuClicked
         }
+    }
+
+    override fun onClick(p0: View?) {
+        TODO("Not yet implemented")
     }
 }
